b447a7f49b17d9ff6f681d95c97d3fd6ef81158b
Mailson Lira <mailson@gmail.com>
2011-10-05 02:55:32 -0300

    Apenas formatação do código

--- src/br/ufpe/cin/rgms/util/Validation.java
+++ src/br/ufpe/cin/rgms/util/Validation.java
@@ -11,15 +11,17 @@ import br.ufpe.cin.rgms.base.IValidator;
 
 public class Validation<T> {
 	private T object;
-	
+
 	public Validation(T object) {
 		this.object = object;
 	}
-	
+
 	public boolean executeValidations() {
 		try {
 			Properties property = new Properties();
-			property.load(this.object.getClass().getResourceAsStream(String.format("%sValidation.properties", this.object.getClass().getSimpleName())));
+			property.load(this.object.getClass().getResourceAsStream(
+					String.format("%sValidation.properties", this.object
+							.getClass().getSimpleName())));
 			Enumeration<Object> enums = property.keys();
 			while (enums.hasMoreElements()) {
 				Object key = enums.nextElement();
@@ -28,15 +30,17 @@ public class Validation<T> {
 				Matcher matcher = pattern.matcher(validationClass);
 				if (matcher.find()) {
 					String className = matcher.group(1);
-					
+
 					Class c = Class.forName(className);
 					IValidator validator = (IValidator) c.newInstance();
-					String campo = (String) this.object.getClass().getMethod(String.format("get%s", key.toString())).invoke(this.object);
-					
+					String campo = (String) this.object.getClass()
+							.getMethod(String.format("get%s", key.toString()))
+							.invoke(this.object);
+
 					return validator.validate(campo);
 				}
 			}
-			
+
 			return true;
 		} catch (IOException e) {
 		} catch (ClassNotFoundException e) {
@@ -47,7 +51,7 @@ public class Validation<T> {
 		} catch (InvocationTargetException e) {
 		} catch (NoSuchMethodException e) {
 		}
-		
+
 		return false;
 	}
 }
