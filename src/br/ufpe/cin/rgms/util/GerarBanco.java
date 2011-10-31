7766679b142f8c78cb7eea40155eab64e4f67d4c
Mailson Lira <mailson@gmail.com>
2011-10-30 22:53:15 -0300

    Dados para popular o banco

--- src/br/ufpe/cin/rgms/util/GerarBanco.java
+++ src/br/ufpe/cin/rgms/util/GerarBanco.java
@@ -12,7 +12,9 @@ import br.ufpe.cin.rgms.base.RGMSException;
 //import br.ufpe.cin.rgms.linhasdepesquisa.modelo.LinhaPesquisa;
 import br.ufpe.cin.rgms.membro.modelo.Estudante;
 import br.ufpe.cin.rgms.membro.modelo.Membro;
+import br.ufpe.cin.rgms.publicacao.MapeamentoTipo;
 import br.ufpe.cin.rgms.publicacao.modelo.ArtigoConferencia;
+import br.ufpe.cin.rgms.publicacao.modelo.ArtigoPeriodico;
 import br.ufpe.cin.rgms.publicacao.modelo.Nivel;
 import br.ufpe.cin.rgms.publicacao.modelo.Publicacao;
 import br.ufpe.cin.rgms.publicacao.modelo.PublicacaoPosGraduacao;
@@ -25,7 +27,7 @@ public class GerarBanco {
 		SchemaExport se = new SchemaExport(conf);
 		se.create(true, true);
 
-		popularBanco();
+		popularBanco2();
 	}
 
 	private static void popularBanco() {
@@ -66,6 +68,202 @@ public class GerarBanco {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public static void popularBanco2() {
+		try {
+			Estudante e1 = new Estudante(
+					"mdlm@cin.ufpe.br", 
+					"Mailson", 
+					"Menezes", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~mdlm/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Fulano de Tal", 
+					"Sicrano");
+			Facade.getInstance().inserirMembro(e1);
+			
+			Estudante e2 = new Estudante(
+					"lqlm@cin.ufpe.br", 
+					"Lucas", 
+					"Queiroz", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~lqlm/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Fulano de Tal", 
+					"Sicrano");
+			Facade.getInstance().inserirMembro(e2);
+			
+			List<Membro> autores1 = new ArrayList<Membro>();
+			autores1.add(Facade.getInstance().getMembro(e1.getEmail()));
+			autores1.add(Facade.getInstance().getMembro(e2.getEmail()));
+			List<String> autoresNaoMembros1 = new ArrayList<String>();
+			autoresNaoMembros1.add("Michelle");
+			ArtigoConferencia a1 = new ArtigoConferencia(
+					autores1, 
+					autoresNaoMembros1, 
+					"Sobre artigos de conferência", 
+					"2011", 
+					null,			// PDF 
+					"ICIP", 
+					"2", 
+					"10", 
+					MapeamentoTipo.CONFERENCIA);
+			Facade.getInstance().inserirPublicacao(a1);
+			
+			// TODO Cadastrar grupo de pesquisa
+			List<Membro> membros1 = new ArrayList<Membro>();
+			membros1.add(Facade.getInstance().getMembro(e1.getEmail()));
+			membros1.add(Facade.getInstance().getMembro(e2.getEmail()));
+			
+			List<Publicacao> artigos1 = new ArrayList<Publicacao>();
+			artigos1.add(Facade.getInstance().getPublicacao(a1.getTitulo()));
+			
+			// Cadastrar o grupo de verdade
+			// membros1 são os membros do grupo
+			// artigos1 são os artigos publicados pelo grupo
+			// Grupo g1 = new Grupo(membros1, artigos1, "Grupo AEIOU", "O grupo estuda os conceitos de grupos de pesquisa");
+			// Facade.getInstance().inserirGrupo(g1);
+			
+			// ---
+			
+			Estudante e3 = new Estudante(
+					"embat@cin.ufpe.br", 
+					"Eduardo", 
+					"Embat", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~embat/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Paulo Gonçalves", 
+					"");
+			Facade.getInstance().inserirMembro(e3);
+			
+			List<Membro> autores3 = new ArrayList<Membro>();
+			autores3.add(Facade.getInstance().getMembro(e3.getEmail()));
+			List<String> autoresNaoMembros3 = new ArrayList<String>();
+			autoresNaoMembros3.add("Amanda");
+			ArtigoPeriodico a3 = new ArtigoPeriodico(
+					autores3, 
+					autoresNaoMembros3, 
+					"Estudo do comportamento de usuários nas mailing-lists", 
+					"2011", 
+					null, 
+					"Whatever Journal", 
+					"3", 
+					"2", 
+					"81-115", 
+					MapeamentoTipo.PERIODICO);
+			Facade.getInstance().inserirPublicacao(a3);
+			
+			Estudante e4 = new Estudante(
+					"jma@cin.ufpe.br", 
+					"Jonathas", 
+					"Alves", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~jma/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Silvio Melo", 
+					"");
+			Facade.getInstance().inserirMembro(e4);
+			
+			Estudante e5 = new Estudante(
+					"evc2@cin.ufpe.br", 
+					"Ellís", 
+					"Queiroz", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~evc2/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Fernando Castor", 
+					"Paulo Borba");
+			Facade.getInstance().inserirMembro(e5);
+			
+			List<Membro> autores4 = new ArrayList<Membro>();
+			autores4.add(Facade.getInstance().getMembro(e4.getEmail()));
+			autores4.add(Facade.getInstance().getMembro(e5.getEmail()));
+			List<String> autoresNaoMembros4 = new ArrayList<String>();
+			PublicacaoPosGraduacao a4 = new PublicacaoPosGraduacao(
+					autores4, 
+					autoresNaoMembros4, 
+					"Desenvolvimento de softwares nas nuvens", 
+					"2010", 
+					null, 
+					"UFPE", 
+					"10", 
+					Nivel.MESTRADO, 
+					MapeamentoTipo.POSGRADUACAO);
+			Facade.getInstance().inserirPublicacao(a4);
+			
+			Estudante e6 = new Estudante(
+					"ass5@cin.ufpe.br", 
+					"Airton", 
+					"Sampaio", 
+					"Estudante", 
+					"CIn", 
+					"UFPE", 
+					"8135555555", 
+					"http://www.cin.ufpe.br/~ass5/", 
+					"Recife", 
+					"Brasil", 
+					"Ativo", 
+					null, 
+					null, 
+					"Fulano de Tal", 
+					"Sicrano");
+			Facade.getInstance().inserirMembro(e6);
+			
+			// TODO Cadastro do grupo de pesquisa
+			List<Membro> membros3 = new ArrayList<Membro>();
+			membros3.add(Facade.getInstance().getMembro(e3.getEmail()));
+			membros3.add(Facade.getInstance().getMembro(e4.getEmail()));
+			membros3.add(Facade.getInstance().getMembro(e5.getEmail()));
+			membros3.add(Facade.getInstance().getMembro(e6.getEmail()));
+			
+			List<Publicacao> artigos3 = new ArrayList<Publicacao>();
+			artigos3.add(Facade.getInstance().getPublicacao(a3.getTitulo()));
+			artigos3.add(Facade.getInstance().getPublicacao(a4.getTitulo()));
+			
+			// TODO Cadastrar o grupo de verdade
+			// Grupo g3 = new Grupo(membros3, artigos3, "CIn Labs", "Grupo de estudo do comportamento das redes do CIn");
+			// Facade.getInstance().inserirGrupo(g3);
+			
+		} catch (RGMSException e) {
+			e.printStackTrace();
+		}
 	}
 	
 
