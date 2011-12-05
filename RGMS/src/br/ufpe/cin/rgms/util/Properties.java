d03eea7074b40a2cfd3a593f5f4710e7765a3ae7
Mailson Lira <mailson@gmail.com>
2011-12-04 22:42:21 -0300

    Modificação de localização
    Signed-off-by: Mailson Lira <mailson@gmail.com>

--- RGMS/src/br/ufpe/cin/rgms/util/Properties.java
+++ RGMS/src/br/ufpe/cin/rgms/util/Properties.java
@@ -6,16 +6,41 @@ import java.util.ResourceBundle;
 import javax.servlet.ServletContext;
 
 public class Properties {
+	private static String language;
+	private static String country;
 
 	private Properties() {
 	}
 
 	private static ResourceBundle bundle;
+	
+	public static void setLocale(String language, String country) {
+		Properties.language = language;
+		Properties.country = country;
+		Properties.bundle = null;
+	}
+	
+	public static void resetLocale() {
+		Properties.setLocale(null, null);
+	}
 
 	public static String getProperty(ServletContext servletContext, String key) {
 		if (bundle == null) {
+			String language;
+			String country;
+			
+			if (Properties.language != null)
+				language = Properties.language;
+			else
+				language = servletContext.getInitParameter("locale.language");
+			
+			if (Properties.country != null)
+				country = Properties.country;
+			else
+				country = servletContext.getInitParameter("locale.country");
+
 			Properties.bundle = ResourceBundle.getBundle("timepesquisa",
-					new Locale(servletContext.getInitParameter("locale.language"), servletContext.getInitParameter("locale.country")));
+					new Locale(language, country));
 		}
 		return bundle.getString(key);
 	}
